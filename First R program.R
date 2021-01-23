6c082d9089c13e361321bb332b5f657844425e8c
Sagars165 <77873128+Sagars165@users.noreply.github.com>
2021-01-23 11:33:45 +0530

    Add files via upload

---
+++ First R program.R
@@ -0,0 +1,45 @@
+2 ^2
+2*pi*6378
+sqrt(10)
+exp(1)
+
+
+
+x<-5
+y<-6
+z<-9
+
+
+a<-x+y+z
+a
+
+
+b<-c(1,2,3,4,5)
+c<-c(2,0,0,4,5)
+
+l<-b+c
+l
+s
+rep(1,8) # this will repeat the first numbers i.e 1 for  8 times
+
+seq(2,5) #this will sequence between 2 to 5 
+
+
+seq(4,20, by=4)  this will show the sequence with gap of 4
+
+b*c
+b/c
+
+sqrt(b)
+
+b[4]
+
+d<-b+c
+d
+
+
+df<-data.frame(x=1:3,y=c("a","b","c"))
+df[1,2]
+df[,2]
+
+df<-[b]
